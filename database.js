8d513feab5c7c8a3a1e3573af4311911f88b3616
Jack Mocherman <mochermanjack@gmail.com>
2016-09-27 13:24:11 -0500

    added mlab to MONGO_URL

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose')
 
-const MONGODB_URL = 'mongodb://localhost:27017/login'
+const MONGODB_URL = 'mongodb://jackmoch:1234@ds041486.mlab.com:41486/login-jackmoch' || 'mongodb://localhost:27017/login'
 
 mongoose.Promise = Promise
 
